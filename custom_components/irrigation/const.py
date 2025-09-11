62ccee86c6589d14757ba3c80cca8cf4bee4559c
James <jbuck2005@hotmail.com>
2025-09-11 09:22:54 -0400

    Update irrigation defaults and services config
    Changed CONF_TOKEN default value and reduced DEFAULT_ZONES from 14 to 4 in const.py. Simplified services.yaml by removing run_zone service definition and adding a placeholder comment.

--- custom_components/irrigation/const.py
+++ custom_components/irrigation/const.py
@@ -5,9 +5,9 @@ from homeassistant.const import CONF_HOST, CONF_PORT, CONF_NAME, CONF_TOKEN
 DOMAIN = "irrigation"
 
 # Configuration keys
-CONF_TOKEN = "changeme-very-secret-token"
+CONF_TOKEN = "token"
 
 # Defaults
 DEFAULT_PORT = 4242
-DEFAULT_ZONES = 14
+DEFAULT_ZONES = 4
 DEFAULT_DURATION = 300
