282f6457ca3ecb58c8d66ce3b84c365c7e0583de
James <jbuck2005@hotmail.com>
2025-09-11 09:39:56 -0400

    Make token a required field in config and options flow
    Changed the token field from optional to required in both the initial configuration and options flow for the irrigation component. This ensures users must provide a token, improving security and configuration consistency.

--- custom_components/irrigation/config_flow.py
+++ custom_components/irrigation/config_flow.py
@@ -41,7 +41,7 @@ class IrrigationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
                 vol.Required("zones", default=DEFAULT_ZONES): int,
                 vol.Required("default_duration", default=DEFAULT_DURATION): int,
-                vol.Optional(CONF_TOKEN, default="changeme-very-secret-token"): str,
+                vol.Required(CONF_TOKEN, default="changeme-very-secret-token"): str,
             }
         )
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
@@ -72,7 +72,7 @@ class IrrigationOptionsFlowHandler(config_entries.OptionsFlow):
                 vol.Required(
                     "default_duration", default=data.get("default_duration", DEFAULT_DURATION)
                 ): int,
-                vol.Optional(CONF_TOKEN, default=data.get("token", "changeme-very-secret-token")): str,
+                vol.Required(CONF_TOKEN, default=data.get("token", "changeme-very-secret-token")): str,
             }
         )
         return self.async_show_form(step_id="init", data_schema=schema)
